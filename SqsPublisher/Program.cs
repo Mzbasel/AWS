df3c03dbd93b56200ffaf6e3ca492dc339e38b3f
Mzbasel <mzbasel637936@gmail.com>
2023-02-03 11:46:11 +0000

    Aws SQS

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "meryem@test.com",
+    FullName = "Meryem Sel",
+    DateOfBirth = new DateTime(1998, 8, 6),
+    GitHubUserName = "Mzbasel"
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer)
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+//Console.Write(response);
